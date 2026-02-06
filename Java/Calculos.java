a9cf33093e8d32c02c3f9df43a1bd1b9bed0ae6a
Diego Montemor <montemordiego@gmail.com>
2026-02-05 22:16:31 -0300

    começo do estudo de java usando o IntelliJ

---
+++ Java/Calculos.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Somar {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro valor: ");
+        int num1 = leitor.nextInt();
+
+        System.out.println("Digite o segundo valor: ");
+        int num2 = leitor.nextInt();
+
+        int soma 
+
+    }
+}
